0bfb3d2be9450fffb5ae2e895ee419db29107021
Richard Yuan <yuanrichard33@gmail.com>
2023-10-12 19:50:13 -0500

    fixing login page

--- goons-app/src/LoginPage.js
+++ goons-app/src/LoginPage.js
@@ -5,9 +5,40 @@ import React, { useState } from 'react';
 function LoginPage() {
     const rightShift = 100; //Change this variable to shift everything right or left
 
-    const loginPressed = (event) => {
-        ;//Send over to backend
+    const [user, setUser] = useState("")
+    const[pass, setPass] = useState("")
 
+    const loginPressed = () => {
+        const jsonString = JSON.stringify({
+            username: user,
+            password: pass
+        });
+        
+        fetch('http://127.0.0.1:5000/login', {
+            mode: 'no-cors',
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: jsonString,
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("hi")
+            console.log(data)
+        })
+        .catch(error => {
+            console.log("bye")
+            console.log(error)
+        });
+    }
+
+    const handleUserChange = (event) => {
+        setUser(event.target.value)
+    }
+
+    const handlePassChange = (event) => {
+        setPass(event.target.value)
     }
     return (
         <div className="Login">
@@ -18,7 +49,7 @@ function LoginPage() {
                 <form>
                     <label style={{marginLeft: rightShift+"px"}}>
                     Username:
-                    <input type="text" style={{marginLeft: "5px"}}/>
+                    <input type="text" style={{marginLeft: "5px"}} placeholder="Enter Username" onChange={handleUserChange}/>
                     </label>
                 </form>
             </div>
@@ -26,7 +57,7 @@ function LoginPage() {
                 <form>
                     <label  style={{marginLeft: rightShift+5+"px"}}>
                     Password:
-                    <input type="password" style={{marginLeft: "5px"}}/>
+                    <input type="password" style={{marginLeft: "5px"}} placeholder="Enter Password" onChange={handlePassChange}/>
                     </label>
                 </form>
             </div>
