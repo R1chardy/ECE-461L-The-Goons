67e92597436cf16507f330e4a7513ad2a46e7e52
Richard Yuan <yuanrichard33@gmail.com>
2023-11-10 12:31:15 -0600

    clean

--- goons-app/src/Projects.js
+++ goons-app/src/Projects.js
@@ -19,7 +19,6 @@ function Projects() {
 
     useEffect(() => {
         document.title = 'Projects';
-        // console.log("hihi")
         //Initialize the page
         fetch('http://127.0.0.1:5000/update_projectpage?username=' + user)
         .then(response => {return response.json()})
